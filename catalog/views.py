f7bf10f2ddf5bd1850af1fa3ec17dc1975bcfac7
Aidar Putilov <putiloff@gmail.com>
2024-09-02 00:21:26 +0500

    Complete in_stock

--- catalog/views.py
+++ catalog/views.py
@@ -12,18 +12,6 @@ class ProductDetail(DetailView):
     model = Product
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Message from {name}(tel.: {phone}): {message}')
-    context = {
-        'title': 'Контакты'
-    }
-    return render(request, 'contacts.html', context)
-
-
 class ProductCreateView(CreateView):
     model = Product
     fields = ('name', 'description', 'category', 'price', 'preview')
@@ -43,11 +31,18 @@ class ProductDeleteView(DeleteView):
 
 def toggle_stock(request, pk):
     product_item = get_object_or_404(Product, pk=pk)
-    if product_item.on_stock:
-        product_item.on_stock = False
-    else:
-        product_item.on_stock = True
-
+    product_item.in_stock = not product_item.in_stock
     product_item.save()
-
     return redirect(reverse('catalog:home'))
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Message from {name}(tel.: {phone}): {message}')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'contacts.html', context)
