ac00822651e1d6eb137521e066bbfd18771093ab
Aidar Putilov <putiloff@gmail.com>
2024-09-02 04:26:33 +0500

    Add Blog in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from catalog.models import Category, Product
+from blog.models import Blog
 
 
 @admin.register(Product)
@@ -8,6 +9,14 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('category',)
     search_fields = ('name', 'description')
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name')
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'created_at', 'is_published', 'view_counter')
+    list_filter = ('is_published',)
+    search_fields = ('title',)
