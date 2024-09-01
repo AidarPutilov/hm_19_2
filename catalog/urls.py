2416182d6c0f0f69fec8d4bd84779bcce5927a75
Aidar Putilov <putiloff@gmail.com>
2024-09-01 14:45:26 +0500

    Complete CBV for Product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from catalog.views import contacts, ProductListView, ProductDetail
+from catalog.views import (contacts,
+                           ProductListView,
+                           ProductDetail,
+                           ProductCreateView,
+                           ProductUpdateView,
+                           ProductDeleteView, toggle_stock)
 from catalog.apps import CatalogConfig
 
 
@@ -8,5 +13,9 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', ProductDetail.as_view(), name='product'),
+    path('product/<int:pk>/', ProductDetail.as_view(), name='view_product'),
+    path('create/', ProductCreateView.as_view(), name='create_product'),
+    path('edit/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+    path('activity/<int:pk>/', toggle_stock, name='toggle_stock'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from catalog.models import Product
+from django.urls import reverse, reverse_lazy
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'home.html'
 
 
 class ProductDetail(DetailView):
     model = Product
-    template_name = 'product.html'
 
 
 def contacts(request):
@@ -23,3 +22,32 @@ def contacts(request):
         'title': 'Контакты'
     }
     return render(request, 'contacts.html', context)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'category', 'price', 'preview')
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'category', 'price', 'preview')
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:home')
+
+
+def toggle_stock(request, pk):
+    product_item = get_object_or_404(Product, pk=pk)
+    if product_item.on_stock:
+        product_item.on_stock = False
+    else:
+        product_item.on_stock = True
+
+    product_item.save()
+
+    return redirect(reverse('catalog:home'))
