11d31d49cb9fa7809ff4055009de8ae90d4fbea0
Aidar Putilov <putiloff@gmail.com>
2024-09-13 01:49:30 +0500

    Add formset for Version

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
 class ProductForm(forms.ModelForm):
@@ -25,3 +25,27 @@ class ProductForm(forms.ModelForm):
         if cleaned_data.lower() in self.forbidden_words:
             raise forms.ValidationError('Ошибка: в описании имеется запрещённое слово')
         return cleaned_data
+
+
+class VersionForm(forms.ModelForm):
+
+    class Meta:
+        model = Version
+        fields = '__all__'
+        # fields = ('name', 'description', 'preview', 'category', 'price', 'in_stock',)
+        # exclude = ('in_stock',)
+
+    # forbidden_words = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+    #                    'бесплатно', 'обман', 'полиция', 'радар')
+    #
+    # def clean_name(self):
+    #     cleaned_data = self.cleaned_data.get('name')
+    #     if cleaned_data.lower() in self.forbidden_words:
+    #         raise forms.ValidationError('Ошибка: в названии имеется запрещённое слово')
+    #     return cleaned_data
+    #
+    # def clean_description(self):
+    #     cleaned_data = self.cleaned_data.get('description')
+    #     if cleaned_data.lower() in self.forbidden_words:
+    #         raise forms.ValidationError('Ошибка: в описании имеется запрещённое слово')
+    #     return cleaned_data
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import inlineformset_factory
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 
-from catalog.forms import ProductForm
-from catalog.models import Product
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Version
 from django.urls import reverse, reverse_lazy
 
 
@@ -29,6 +30,37 @@ class ProductUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('catalog:view_product', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        ProductFormset = inlineformset_factory(
+            Product,
+            Version,
+            VersionForm,
+            extra=1
+        )
+        if self.request.method == 'POST':
+            context_data['formset'] = ProductFormset(
+                self.request.POST,
+                instance=self.object
+            )
+        else:
+            context_data['formset'] = ProductFormset(
+                instance=self.object
+            )
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
 
 
 class ProductDeleteView(DeleteView):
