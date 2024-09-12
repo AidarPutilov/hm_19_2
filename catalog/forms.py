eb6b71f4f39f26ecf2a457dbcb412b384dd75621
Aidar Putilov <putiloff@gmail.com>
2024-09-12 22:24:05 +0500

    Complete forms for Product

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,5 +8,20 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         # fields = '__all__'
-        fields = ('name', 'description', 'preview', 'category', 'price',)
+        fields = ('name', 'description', 'preview', 'category', 'price', 'in_stock',)
         # exclude = ('in_stock',)
+
+    forbidden_words = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+                       'бесплатно', 'обман', 'полиция', 'радар')
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get('name')
+        if cleaned_data.lower() in self.forbidden_words:
+            raise forms.ValidationError('Ошибка: в названии имеется запрещённое слово')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get('description')
+        if cleaned_data.lower() in self.forbidden_words:
+            raise forms.ValidationError('Ошибка: в описании имеется запрещённое слово')
+        return cleaned_data
